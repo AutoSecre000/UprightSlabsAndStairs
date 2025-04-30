f70ac13f8105bc0bb3cf667063959e32375cd958
Bei Dou <109635167+AutoSecre000@users.noreply.github.com>
2025-04-30 23:17:51 +0800

    Init fabric project repo.

---
+++ src/client/kotlin/com/autosec/onecore/uss/UprightSlabsStairsClient.kt
@@ -0,0 +1,9 @@
+package com.autosec.onecore.uss
+
+import net.fabricmc.api.ClientModInitializer
+
+object UprightSlabsStairsClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/kotlin/com/autosec/onecore/uss/UprightSlabsStairsDataGenerator.kt
@@ -0,0 +1,9 @@
+package com.autosec.onecore.uss
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object UprightSlabsStairsDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
