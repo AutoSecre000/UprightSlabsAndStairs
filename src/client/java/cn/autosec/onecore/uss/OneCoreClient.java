086f20a5586a174ce86e21994bb87a1b3458a253
Bei Dou <109635167+AutoSecre000@users.noreply.github.com>
2025-05-11 12:14:33 +0800

    Add stained-glass staff.

--- src/client/java/cn/autosec/onecore/uss/OneCoreClient.java
+++ src/client/java/cn/autosec/onecore/uss/OneCoreClient.java
@@ -1,10 +1,22 @@
 package cn.autosec.onecore.uss;
 
+import cn.autosec.onecore.uss.definition.lib.BlockLib;
+import cn.autosec.onecore.uss.registry.ModBlocks;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+
+import java.util.List;
 
 public class OneCoreClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		List<BlockLib> blockLibs = ModBlocks.getBlockLib();
+		for (BlockLib lib : blockLibs) {
+			if (lib.isGlass) {
+				BlockRenderLayerMap.INSTANCE.putBlock(lib.modRegistry.get(), lib.cutout ? RenderLayer.getCutout() : RenderLayer.getTranslucent());
+			}
+		}
 	}
 }
